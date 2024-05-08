63629a1dcc32facd1736cab34a16a24f51227fde
ashaheer-mufin <150656949+ashaheer-mufin@users.noreply.github.com>
2024-05-08 13:40:48 +0500

    Production fixes (#4)
    * Fixes
    
    * FIxes

--- src/Components/Cargo/CardDeck/CargoCardDeck.tsx
+++ src/Components/Cargo/CardDeck/CargoCardDeck.tsx
@@ -1,80 +1,80 @@
-import React from "react";
+import React from 'react';
 
-import "../../../app/styles/cards.css";
-import stepOne from "../../../Assets/Front 3.svg";
-import steptwo from "../../../Assets/Front 4.svg";
-import stepUnion from "../../../Assets/StepUnion.svg";
-import Image from "next/image";
+import '../../../app/styles/cards.css';
+import stepOne from '../../../Assets/Front 3.svg';
+import steptwo from '../../../Assets/Front 4.svg';
+import stepUnion from '../../../Assets/StepUnion.svg';
+import Image from 'next/image';
 const CardDeck = () => {
   return (
     <>
-      <div className="wrap">
-        <div className="section-wrap section-wrap-01" id="service">
-          <section className="section-02 bg-gradient-to-br from-purple-600 to-purple-300  absolute py-10">
-            <ul className="step-card-list  ">
-              <li className="reactive h-[700px]">
-                <div className="contents">
-                  <div className="w-[620px] mt-[75px]  ml-[80px]">
-                    <h1 className=" text-[68px] font-bold  text-[##0E121B]">
+      <div className='wrap'>
+        <div className='section-wrap section-wrap-01' id='service'>
+          <section className='section-02 bg-gradient-to-br from-purple-600 to-purple-300  absolute py-10'>
+            <ul className='step-card-list  '>
+              <li className='reactive h-[700px]'>
+                <div className='contents'>
+                  <div className='w-[620px] mt-[75px]  ml-[80px]'>
+                    <h1 className=' text-[68px] font-bold  text-[##0E121B]'>
                       화물차 랩핑으로
-                      <br/>
-                      <span className="text-[#561AA4]">
+                      <br />
+                      <span className='text-[#561AA4]'>
                         사업을 확장시켜보세요
                       </span>
                     </h1>
-                    <p className="text-[#101440] text-[32px] font-bold w-[480px]">
+                    <p className='text-[#101440] text-[32px] font-bold w-[480px]'>
                       빠른 광고 신청과 계약 관리로 간편하게 믿고 관리하세요.
                     </p>
                   </div>
 
-                  <div className="mt-16">
+                  <div className='mt-16'>
                     <Image
                       src={stepOne}
-                      alt="Step 1 image"
-                      className="w-[510px] h-[578px]"
+                      alt='Step 1 image'
+                      className='w-[510px] h-[578px]'
                     />
                   </div>
                 </div>
-                <div className="step-num flex justify-center items-center">
+                <div className='step-num flex justify-center items-center'>
                   <div>
                     <Image
                       src={stepUnion}
-                      alt="icon"
-                      className="w-[20px] h-[24px] mr-1"
+                      alt='icon'
+                      className='w-[20px] h-[24px] mr-1'
                     />
                   </div>
                   <div>STEP-1</div>
                 </div>
               </li>
-              <li className="reactive h-[700px]">
-                <div className="contents">
-                  <div className="w-[620px] mt-[75px]  ml-[80px]">
-                  <h1 className=" text-[68px] font-bold  text-[##0E121B]">
+              <li className='reactive h-[700px]'>
+                <div className='contents'>
+                  <div className='w-[620px] mt-[75px]  ml-[80px]'>
+                    <h1 className=' text-[68px] font-bold  text-[##0E121B]'>
                       화물차 랩핑으로
-                      <br/>
-                      <span className="text-[#561AA4]">
+                      <br />
+                      <span className='text-[#561AA4]'>
                         사업을 확장시켜보세요
                       </span>
                     </h1>
-                  
-                    <p className="text-[#101440] text-[32px] font-bold w-[480px]">
+
+                    <p className='text-[#101440] text-[32px] font-bold w-[480px]'>
                       빠른 광고 신청과 계약 관리로 간편하게 믿고 관리하세요.
                     </p>
                   </div>
-                  <div className="mt-16">
+                  <div className='mt-16'>
                     <Image
                       src={steptwo}
-                      alt="Step 2 image"
-                      className="w-[510px] h-[578px]"
+                      alt='Step 2 image'
+                      className='w-[510px] h-[578px]'
                     />
                   </div>
                 </div>
-                <div className="step-num flex justify-center items-center">
+                <div className='step-num flex justify-center items-center'>
                   <div>
                     <Image
                       src={stepUnion}
-                      alt="icon"
-                      className="w-[20px] h-[24px] mr-1"
+                      alt='icon'
+                      className='w-[20px] h-[24px] mr-1'
                     />
                   </div>
                   <div>STEP-2</div>
