05170886dbdfa2d43e852f8dae739f938a134ef5
MD. Fahim Ahmed Sajib <61555992+sfahim-mufin@users.noreply.github.com>
2024-08-10 12:40:12 +0600

    [IC-1750] 👑 Improvement: Update Footer Text (#40)
    * text updated
    
    * typo fixed

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -13,19 +13,19 @@ interface Props {
 
 const Footer = ({ bgColor, type = 'dark', page = 'cargo' }: Props) => {
   const FooterListPage = [
-    { id: 1, lable: '개인정보 처리방침', href: '/privacy-policy' },
-    { id: 2, lable: '서비스 이용약관', href: '/terms-of-services' },
-    { id: 3, lable: '위치정보 수집약관', href: '/location-information-terms' },
+    { id: 1, label: '개인정보 처리방침', href: '/privacy-policy' },
+    { id: 2, label: '서비스 이용약관', href: '/terms-of-services' },
+    { id: 3, label: '위치정보 수집약관', href: '/location-information-terms' },
     {
       id: 4,
-      lable: '개인정보 수집 및 이용동의',
+      label: '개인정보 수집 및 이용동의',
       href: '/personal-information-agreement',
     },
   ];
   const FooterItems = [
-    { id: 1, lable: '상호: (주)머스트핀테크 | 대표자: 차주헌' },
-    { id: 2, lable: '주소: 서울특별시 강남구 테헤란로 418 다봉타워빌딩 11층 1115호 1116호' },
-    { id: 3, lable: '사업자 등록번호: 312-88-01481 | 이메일: cs@moad.co.kr' },
+    { id: 1, label: '상호: 주식회사 슈퍼트러스트 (SUPER TRUST) | 대표자: 최병호' },
+    { id: 2, label: '주소: 서울특별시 송파구 송파대로 167, 에이동 917호 (문정동, 문정역 테라타워)' },
+    { id: 3, label: '사업자 등록번호: 559-87-02646 | 이메일: cs@moad.co.kr' },
   ];
   return (
     <footer style={{ backgroundColor: bgColor ? bgColor : '' }} className='relative px-4 md:px-6 lg:px-10 xl:px-28 h-full  flex flex-col justify-center py-6'>
@@ -46,7 +46,7 @@ const Footer = ({ bgColor, type = 'dark', page = 'cargo' }: Props) => {
                 key={item.id}
                 className="text-sm md:text-base  text-start font-Pretendard ">
                 <Link href={`/${page}${item.href}`} className={` ${type === 'dark' ? 'text-gray-30' : 'text-gray-60'} `}>
-                  {item.lable}
+                  {item.label}
                 </Link>
               </button>
             ))}
@@ -57,7 +57,7 @@ const Footer = ({ bgColor, type = 'dark', page = 'cargo' }: Props) => {
             <li
               key={item.id}
               className={` ${type === 'dark' ? 'text-gray-30' : 'text-gray-60'}  text-sm md:text-base  font-Pretendard `}>
-              {item.lable}
+              {item.label}
             </li>
           ))}
         </ul>
